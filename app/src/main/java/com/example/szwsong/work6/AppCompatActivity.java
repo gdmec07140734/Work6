ffce5060207b25ec3cd52e3f520145a81f62cd23
gdmec07140734 <2295883498@qq.com>
2015-12-11 16:56:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/szwsong/work6/AppCompatActivity.java
@@ -0,0 +1,12 @@
+package com.example.szwsong.work6;
+
+import android.os.Bundle;
+
+/**
+ * Created by JACKY.YAM on 2015/12/11.
+ */
+public class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+}
